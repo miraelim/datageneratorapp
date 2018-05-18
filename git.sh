e516a05f927209b2094fa4185fa974ac88ed07d4
miraelim <lovemirae94@gmail.com>
2018-05-18 16:53:15 +0900

    weatherdatapointfinal_4

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ git init
 git remote add origin https://github.com/miraelim/datageneratorapp.git
 git add .
 git commit -m "weatherdatapointfinal_4"
-git push -u origin master
+git push -u origin master -f
 git remote rm origin
 git reset
 
